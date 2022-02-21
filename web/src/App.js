2b85fe48145209652a0c0b1650320cdfd43ae0db
Lily Li <lilyli@Lilys-Mac-mini.local>
2022-02-20 23:48:31 -0800

    set up framework

---
+++ web/src/App.js
@@ -0,0 +1,12 @@
+import HomePage from "./components/HomePage";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Lily website setup :-)</h1>
+      <HomePage></HomePage>
+    </div>
+  );
+}
+
+export default App;
---
+++ web/src/components/HomePage.js
@@ -0,0 +1,12 @@
+import Navbar from "./Navbar"
+
+function HomePage(){
+     return (
+          <div>
+               <Navbar></Navbar>
+               <h1>This is HomePage</h1>
+          </div>
+     )
+}
+
+export default HomePage;
